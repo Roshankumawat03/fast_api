81c75247744bd6f8c3b9d372735d68a034def6b7
Roshan Kumawat <roshankumawat529@gmail.com>
2025-12-24 15:06:26 +0530

    curd-basemodel...

--- curd_basemodel.py
+++ curd_basemodel.py
@@ -47,10 +47,14 @@ def update(data: User, response:Response):
     existing_data = read_file()
     for x in existing_data:
         if x["username"] == data.username:
-            x["name"] = data.name
-            x["mail"] = data.mail
-            x["addr"] = data.addr
-            x["mob"] = data.mob
+            if data.name:
+                x["name"] = data.name
+            if data.mail:
+                x["mail"] = data.mail
+            if data.addr:    
+                x["addr"] = data.addr
+            if data.mob:    
+                x["mob"] = data.mob
             write_file(existing_data)
             return{"message": "username updated done."}
 
