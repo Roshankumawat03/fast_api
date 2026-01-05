9c7cb15f76769d83c61089618f784be4cdf7ee28
Roshan Kumawat <roshankumawat529@gmail.com>
2026-01-05 15:40:05 +0530

    JWT Token and Using inbuilt form for Auth

--- security_frist_steps.py
+++ security_frist_steps.py
@@ -1,27 +1,52 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, Response, status, Form
+from fastapi import Depends, FastAPI, Response, status, Form, HTTPException
+from fastapi.responses import HTMLResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 class Dummy(BaseModel):
     username: str
     password: str
     client_id: str
+ 
 
-
-@app.get("/login")
+@app.post("/test")
 def login(data: Annotated[Dummy, Form()], response:Response):
     if data.username == "admin" and data.password == "123":
-        return {"message": "Login done"}
+        return {"message": "login done."}
+    else:
+        response.status_code = status.HTTP_401_UNAUTHORIZED
+        return {"message": "login fail"}
+    
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+@app.get("/login")
+def login(data: Annotated[OAuth2PasswordRequestForm, Depends()], response:Response):
+    if data.username == "admin" and data.password == "123":
+        return {"access_token": data.username, "token_type": "bearer"}
     else:
         Response.status_code = status.HTTP_401_UNAUTHORIZED
         return{"message": "login fail"}
 
 @app.get("/items/")
-def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
-    return {"token": token}
+def read_items(data_form_login_api: Annotated[str, Depends(oauth2_scheme)]):
+    return {"data_form_login_api": data_form_login_api}
+
+
+@app.get("/home")
+def read_items(data_form_login_api: Annotated[str, Depends(oauth2_scheme)]):
+    return {"message": "Home page"}
+
+
+@app.get("/")
+def home():
+    return HTMLResponse("""
+    <h1>Hi</h1>
+    <h2>Hi</h2>
+    """)
