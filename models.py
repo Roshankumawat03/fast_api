d64edfcd9e7e4070892f11d41c91afa8e6c56b0d
Roshan Kumawat <roshankumawat529@gmail.com>
2026-01-07 13:54:58 +0530

    Using DB to Store and Get Data

--- models.py
+++ models.py
@@ -1,15 +1,18 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Body
 from sqlmodel import SQLModel, Field, create_engine, Session, select
+import bcrypt
 
-app = FastAPI()
+
 
 class Hero(SQLModel, table= True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field()
-    age: int | None = Field()
-    secret_name: str
+    username: str = Field(unique=True, nullable=False)
+    mob: str = Field(unique=True, nullable=False)
+    age: int 
+    password: str = Field(nullable=False)
 
 
 sqlite_url = f"sqlite:///database.db"
@@ -21,3 +24,31 @@ SQLModel.metadata.create_all(engine)
 def get_session():
     with Session(engine) as session:
         yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "This is home page."}
+
+@app.post("/register")
+def register(db: SessionDep, data = Body(...)):
+    password = bcrypt.hashpw(data.get("password").encode("UTF-8"), salt=bcrypt.gensalt(14))
+    data_for_entry = Hero(
+        name=data.get("name"),
+        username=data.get("username"),
+        mob=data.get("mob"),
+        age=data.get("age"),
+        password=password
+    )
+    db.add(data_for_entry)
+    db.commit()
+    return {"message": data}
+
+
+@app.post("/get_all_data")
+def register(db : SessionDep):
+    test = db.query(Hero).all()
+    return {"message": test}
